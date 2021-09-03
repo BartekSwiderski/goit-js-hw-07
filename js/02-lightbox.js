6e3973a555215e40fa141c6ce18e94076bd1ca2f
Bartek Świderski <bartekzg@gmail.com>
2021-09-03 18:37:35 +0200

    poprawki esc

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+const qs = (selector) => document.querySelector(selector);
+const galleryGrid = qs(".gallery")
+const prev = galleryItems.map(prev=>prev.preview);
+const oryg = galleryItems.map(org=>org.original);
+const desc = galleryItems.map(des=>des.description);
+let galleryMarkup = "";
+for (let i = 0; i<prev.length; i++){
+galleryMarkup+=`<a class="gallery__item" href="${oryg[i]}">
+<img class="gallery__image" src="${prev[i]}" alt="${desc[i]}"  />
+</a>`
+}
+galleryGrid.insertAdjacentHTML("beforeend", galleryMarkup);
+let gallery = new SimpleLightbox('.gallery a',{captionsData:"alt", captionDelay:"250"});
+gallery.on('show.simplelightbox', function () {
+	preventDefault()
+});
+galleryGrid.addEventListener("click", preventDefault())
